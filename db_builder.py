cb33ad5ed05c2fc5b3c9d1de01352eacca2c5e1a
lilharry <hlum1@stuy.edu>
2017-02-05 21:16:39 -0500

    done

--- db_builder.py
+++ db_builder.py
@@ -5,14 +5,13 @@ server = MongoClient('149.89.150.100')
 
 db = server.celharry
 
+db.students.drop()
+
 students = db.students
+
 
 peeps = open("peeps.csv") 
 peepdict = csv.DictReader(peeps)
-
-courses = open("courses.csv") 
-coursedict = csv.DictReader(courses)
-
 
 for student in peepdict:
     info = {}
@@ -20,12 +19,17 @@ for student in peepdict:
     info['id'] = student['id']
     info['age'] = student['age']
     courses = {}
+
+    cours = open("courses.csv") 
     
-    for item in coursedict:
+    coursedict = csv.DictReader(cours)    
+
+    for key in coursedict:
         print(student['id'])
-        print(item['id'])
-        if student['id'] == item['id']:
-            courses[item['code']] = item['mark']
+
+        print(key['id'])
+        if student['id'] == key['id']:
+            courses[key['code']] = key['mark']
             
     info['courses'] = courses
     
