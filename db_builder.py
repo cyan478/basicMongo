2a49908a83dd8e4c88b129b33c2da0f7a8cbba6a
Harry <lilharry221@gmail.com>
2017-02-05 20:11:53 -0500

    almost done

---
+++ db_builder.py
@@ -0,0 +1,30 @@
+import csv
+from pymongo import MongoClient
+
+server = MongoClient('149.89.150.100')
+
+db = server.celharry
+
+students = db.students
+
+peeps = open("peeps.csv") 
+peepdict = csv.DictReader(peeps)
+
+courses = open("courses.csv") 
+coursedict = csv.DictReader(courses)
+
+
+for student in peepdict:
+    info = {}
+    info['name'] = student['name']
+    info['id'] = student['id']
+    info['age'] = student['age']
+    courses = {}
+    
+    for item in coursedict:
+        if student['id'] == item['id']:
+            courses[item['code']] = item['mark']
+            
+    info['courses'] = courses
+    
+    students.insert_one(info)
